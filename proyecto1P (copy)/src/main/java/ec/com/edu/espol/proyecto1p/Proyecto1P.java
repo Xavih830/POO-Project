9e84533e070a92ffd15f06c9527b791138abe477
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2023-12-02 12:04:08 -0500

    Add files via upload

---
+++ proyecto1P (copy)/src/main/java/ec/com/edu/espol/proyecto1p/Proyecto1P.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.com.edu.espol.proyecto1p;
+
+/**
+ *
+ * @author xavih830
+ */
+public class Proyecto1P {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
